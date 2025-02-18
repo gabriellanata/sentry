445c7e70f369dd05156667abd52ed947806cf500
Katie Byers <lobsterkatie@gmail.com>
2025-02-18 12:34:25 -0800

    update grouphashes gathered with old schemata

--- src/sentry/grouping/ingest/grouphash_metadata.py
+++ src/sentry/grouping/ingest/grouphash_metadata.py
@@ -146,6 +146,7 @@ def create_or_update_grouphash_metadata_if_needed(
 
         GroupHashMetadata.objects.create(**new_data)
 
+    # Update data in existing metadata record if needed
     else:
         updated_data: dict[str, Any] = {}
 
@@ -158,6 +159,25 @@ def create_or_update_grouphash_metadata_if_needed(
                 "current_config": grouphash.metadata.latest_grouping_config,
                 "new_config": grouping_config,
             }
+
+        # If the metadata was gathered under an old schema, get new data and bump the schema version
+        if grouphash.metadata.schema_version != GROUPHASH_METADATA_SCHEMA_VERSION:
+            updated_data.update(
+                # This includes `schema_version`
+                get_grouphash_metadata_data(event, project, variants, grouping_config)
+            )
+
+            db_hit_metadata.update(
+                {
+                    "reason": (
+                        "outdated_schema"
+                        if not db_hit_metadata.get("reason")
+                        else "config_and_schema"
+                    ),
+                    "current_version": grouphash.metadata.schema_version,
+                    "new_version": GROUPHASH_METADATA_SCHEMA_VERSION,
+                }
+            )
 
         # Only hit the DB if there's something to change
         if updated_data:
